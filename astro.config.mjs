88df873d8cdcbd175cd4d863f5894fbb45f08be8
Hans Kuijpers <hans2103@gmail.com>
2024-01-24 15:45:14 +0100

    refactor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,36 @@
 import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  i18n: {
+    defaultLocale: 'nl',
+    locales: ['nl'],
+    showDefaultLocale: true
+  },
+  integrations: [starlight({
+    title: 'Xpèr Products',
+    locales: {
+      root: {
+        label: "Nederlands",
+        lang: "nl"
+      }
+    },
+    sidebar: [{
+      label: 'Guides',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Example Guide',
+        link: '/guides/example/'
+      }]
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }]
+  }), tailwind()]
 });
--- src/pages/index.md
+++
@@ -1,7 +0,0 @@
----
-layout: '../layouts/Layout.astro'
----
-
-# test
-
-test
